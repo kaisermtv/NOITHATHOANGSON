571c5f3c53ee78baeec50b925ef555eca9a17b1b
kaisermtv <kaisermtv@yahoo.com>
2017-09-28 11:15:58 +0700

    sfsdv

--- Controller/Menu.ascx.cs
+++ Controller/Menu.ascx.cs
@@ -18,7 +18,7 @@ public partial class Controller_Menu : System.Web.UI.UserControl
     protected void Page_Load(object sender, EventArgs e)
     {
         DataTable objData = objMenu.getList(0, 1, true);
-
+        string a = objData.Rows[4]["COUNTCHILD"].ToString();
         dtlMenu.DataSource = objData.DefaultView;
         dtlMenu.DataBind();
 
